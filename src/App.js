1bf492d837fbb977047bc99c046ad63c4113ac07
michele de bruyn <debruyn.mm@gmail.com>
2017-03-17 15:07:11 +0100

    Add logic to add project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+      // Own data: not a clean way to place data here, better to
+      // create projects: and than pass the data through
+      // ComponentWillMount
+      ]
+    }
+  }
+
+  componentWillMount() {
+    // Place to call api for data
+    // lifecycle method, to check when it fires off
+    this.setState({projects: [
+      {
+        title: "Business Website",
+        category: "Web design"
+
+      },
+      {
+        title: "Travel Website",
+        category: "Travel Agency"
+      },
+      {
+        title: "Monkey Business",
+        category: "Non Sense"
+      }
+      ]});
+  };
+handleAddProject(project){
+  // here add to the state of the main component
+  let projects = this.state.projects;
+  projects.push(project);
+  this.setState({projects: projects});
+}
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      < AddProject addProject={this.handleAddProject.bind(this)}/>
+      < Projects projects={this.state.projects} />
       </div>
-    );
+      );
   }
 }
 
