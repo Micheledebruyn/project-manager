458df500869c8e2420e12ecddce7d46a0bd1c64c
michele de bruyn <debruyn.mm@gmail.com>
2017-03-18 11:31:21 +0100

    Add destroy & id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 
@@ -21,33 +22,43 @@ class App extends Component {
     // lifecycle method, to check when it fires off
     this.setState({projects: [
       {
+        id: uuid.v4(),
         title: "Business Website",
         category: "Web design"
 
       },
       {
+        id: uuid.v4(),
         title: "Travel Website",
         category: "Travel Agency"
       },
       {
+        id: uuid.v4(),
         title: "Monkey Business",
         category: "Non Sense"
       }
       ]});
   };
-handleAddProject(project){
-  // here add to the state of the main component
-  let projects = this.state.projects;
-  projects.push(project);
-  this.setState({projects: projects});
-}
 
+  handleAddProject(project){
+    // here add to the state of the main component
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects: projects});
+  }
 
   render() {
     return (
       <div className="App">
       < AddProject addProject={this.handleAddProject.bind(this)}/>
-      < Projects projects={this.state.projects} />
+      < Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
       </div>
       );
   }
